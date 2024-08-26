f25424b076bfffb5eea1fd43491563f7e07e9934
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-26 09:49:08 +0000

    Updating to latest FlutterFlow output.

--- lib/car_seized/customer_car_deails_picture_step1/customer_car_deails_picture_step1_widget.dart
+++ lib/car_seized/customer_car_deails_picture_step1/customer_car_deails_picture_step1_widget.dart
@@ -1022,8 +1022,11 @@ class _CustomerCarDeailsPictureStep1WidgetState
                                         type: PageTransitionType.fade,
                                         child: FlutterFlowExpandedImageView(
                                           image: Image.memory(
-                                            _model.image1File?.bytes ??
-                                                Uint8List.fromList([]),
+                                            true
+                                                ? _model.image1File
+                                                : _model.noImageAvailableFile
+                                                        ?.bytes ??
+                                                    Uint8List.fromList([]),
                                             fit: BoxFit.contain,
                                           ),
                                           allowRotation: false,
@@ -1039,8 +1042,11 @@ class _CustomerCarDeailsPictureStep1WidgetState
                                     child: ClipRRect(
                                       borderRadius: BorderRadius.circular(8.0),
                                       child: Image.memory(
-                                        _model.image1File?.bytes ??
-                                            Uint8List.fromList([]),
+                                        true
+                                            ? _model.image1File
+                                            : _model.noImageAvailableFile
+                                                    ?.bytes ??
+                                                Uint8List.fromList([]),
                                         width: 200.0,
                                         height: 200.0,
                                         fit: BoxFit.cover,
