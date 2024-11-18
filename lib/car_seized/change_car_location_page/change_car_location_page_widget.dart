5011e83f3e936e21b5b0ec4be88672b856bff636
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-18 01:24:11 +0000

    Updating to latest FlutterFlow output.

--- lib/car_seized/change_car_location_page/change_car_location_page_widget.dart
+++ lib/car_seized/change_car_location_page/change_car_location_page_widget.dart
@@ -129,26 +129,26 @@ class _ChangeCarLocationPageWidgetState
             _model.priceTextFieldTextController?.text =
                 functions.removeCommaFromNumText(
                     _model.priceTextFieldTextController.text);
-            // _model.priceTextFieldFocusNode?.requestFocus();
-            // WidgetsBinding.instance.addPostFrameCallback((_) {
+            _model.priceTextFieldFocusNode?.requestFocus();
+            WidgetsBinding.instance.addPostFrameCallback((_) {
               _model.priceTextFieldTextController?.selection =
                   TextSelection.collapsed(
                 offset: _model.priceTextFieldTextController!.text.length,
               );
-            // });
+            });
           });
         } else {
           safeSetState(() {
             _model.priceTextFieldTextController?.text =
                 functions.returnNumberWithComma2Decimal(
                     _model.priceTextFieldTextController.text)!;
-            // _model.priceTextFieldFocusNode?.requestFocus();
-            // WidgetsBinding.instance.addPostFrameCallback((_) {
+            _model.priceTextFieldFocusNode?.requestFocus();
+            WidgetsBinding.instance.addPostFrameCallback((_) {
               _model.priceTextFieldTextController?.selection =
                   TextSelection.collapsed(
                 offset: _model.priceTextFieldTextController!.text.length,
               );
-            // });
+            });
           });
         }
       },
