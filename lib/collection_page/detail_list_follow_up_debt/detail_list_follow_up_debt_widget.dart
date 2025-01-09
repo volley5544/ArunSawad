5dd2853860b0cae2a9ad406de1fc1472fd7c2fcf
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-09 04:17:11 +0000

    Updating to latest FlutterFlow output.

--- lib/collection_page/detail_list_follow_up_debt/detail_list_follow_up_debt_widget.dart
+++ lib/collection_page/detail_list_follow_up_debt/detail_list_follow_up_debt_widget.dart
@@ -157,6 +157,7 @@ class _DetailListFollowUpDebtWidgetState
       }
 
       if (widget!.fromIconCall!) {
+        await Future.delayed(const Duration(milliseconds: 2000));
         if (!functions
             .checkPhoneNumberChar(CollectionApiGetDataPersonCall.mobilenumber(
           (_model.getListDataPerson?.jsonBody ?? ''),
