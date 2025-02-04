9c7156a4ec13bab77243117d143d840bc692b5ed
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-04 15:20:40 +0000

    Updating to latest FlutterFlow output.

--- lib/tracking_emp_page/tracking_emp_page_widget.dart
+++ lib/tracking_emp_page/tracking_emp_page_widget.dart
@@ -113,7 +113,7 @@ class _TrackingEmpPageWidgetState extends State<TrackingEmpPageWidget>
           .toList()
           .toList()
           .cast<String>();
-      safeSetState(() {});
+      FFAppState().update(() {});
       Navigator.pop(context);
     });
 
