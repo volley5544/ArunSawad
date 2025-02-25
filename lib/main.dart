073631904192a7ea3cf1314662b102fe24cb0c0c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-25 16:40:04 +0000

    Updating to latest FlutterFlow output.

--- lib/main.dart
+++ lib/main.dart
@@ -113,6 +113,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'ArunSawad',
       localizationsDelegates: [
         FFLocalizationsDelegate(),
