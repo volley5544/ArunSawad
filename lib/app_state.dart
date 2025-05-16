dc24e089f509f11872b7919a0fdd2c895203ffb2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-05-16 02:06:53 +0000

    Updating to latest FlutterFlow output.

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -10101,7 +10101,7 @@ class FFAppState extends ChangeNotifier {
     _groceryAssignUrlDocRef = value;
   }
 
-  bool _isProductionNew = false;
+  bool _isProductionNew = true;
   bool get isProductionNew => _isProductionNew;
   set isProductionNew(bool value) {
     _isProductionNew = value;
