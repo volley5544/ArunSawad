8a62f614fb6f3ad8cf3961b76f75fe710405b405
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-12-29 08:03:11 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/profile_page_folder/setting_app_page/setting_app_page_model.dart
+++ lib/pages/profile_page_folder/setting_app_page/setting_app_page_model.dart
@@ -1,45 +1,15 @@
-import '/auth/firebase_auth/auth_util.dart';
-import '/backend/backend.dart';
-import '/components/loading_scene/loading_scene_widget.dart';
-import '/flutter_flow/flutter_flow_animations.dart';
-import '/flutter_flow/flutter_flow_expanded_image_view.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/flutter_flow/upload_data.dart';
-import 'dart:math';
-import 'dart:ui';
-import '/custom_code/actions/index.dart' as actions;
-import '/flutter_flow/custom_functions.dart' as functions;
 import '/index.dart';
 import 'setting_app_page_widget.dart' show SettingAppPageWidget;
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
-import 'package:webviewx_plus/webviewx_plus.dart';
 
 class SettingAppPageModel extends FlutterFlowModel<SettingAppPageWidget> {
-  ///  State fields for stateful widgets in this page.
-
-  bool isDataUploading_uploadProfileImageSettingPage = false;
-  FFUploadedFile uploadedLocalFile_uploadProfileImageSettingPage =
-      FFUploadedFile(bytes: Uint8List.fromList([]), originalFilename: '');
-
-  // Stores action output result for [Custom Action - uploadFileFirebaseStorage] action in IconButton widget.
-  String? uploadFirebaseStorageAction;
-  // Stores action output result for [Custom Action - checkImageIsAvailable] action in IconButton widget.
-  bool? checkProfileImgIsAvailable;
-  // State field(s) for SwitchListTile widget.
-  bool? switchListTileValue;
-
   @override
   void initState(BuildContext context) {}
 
